53d317aba5762cae7754afbf84adb51a474ecba3
madhumitha27 <aparabm@gmail.com>
2020-09-25 15:36:44 -0500

    my last change

--- clientms/settings.py
+++ clientms/settings.py
@@ -136,11 +136,11 @@ LOGIN_URL = 'login'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environget('EMAIL_HOST')
-EMAIL_HOST_USER = os.environget('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD =os.environget('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = os.environget('EMAIL_PORT')
-EMAIL_USE_TLS = os.environget('EMAIL_USE_TLS')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 django_heroku.settings(locals())
 
