faa2daf4e7b8de96f752b037e1a4929c7b74a653
madhumitha27 <aparabm@gmail.com>
2020-09-25 15:30:19 -0500

    change in setting file

--- clientms/settings.py
+++ clientms/settings.py
@@ -134,12 +134,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'madaad87@gmail.com'
-EMAIL_HOST_PASSWORD = 'testpassword@123'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environget('EMAIL_HOST')
+EMAIL_HOST_USER = os.environget('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =os.environget('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environget('EMAIL_PORT')
+EMAIL_USE_TLS = os.environget('EMAIL_USE_TLS')
 
 django_heroku.settings(locals())
 
